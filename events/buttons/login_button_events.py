53c855f678ef29a27a625e56b94727845b07425e
8FAX <liam@uilliam.com>
2024-03-04 00:36:48 -0500

    updated login to use the pw server

--- events/buttons/login_button_events.py
+++ events/buttons/login_button_events.py
@@ -6,11 +6,12 @@ from events.client.PWauthentication import authenticate
 def on_login_button_clicked(login_page):
     username = login_page.username_input.text()
     password = login_page.password_input.text()
-    if not authenticate(username, password):
+    data = authenticate(username, password)
+    if data == True:
+        login_page.stacked_widget.setCurrentIndex(1) # Switch to the notes page
+    else:
         login_page.password_input.setStyleSheet(login_input_style_fail)
         QTimer.singleShot(3000, lambda: reset_password_field(login_page))
-    else:
-        login_page.stacked_widget.setCurrentIndex(1) # Switch to the notes page
         
 def reset_password_field(login_page):
     login_page.password_input.setStyleSheet(login_input_style)
--- events/client/PWauthentication.py
+++ events/client/PWauthentication.py
@@ -1,10 +1,23 @@
-import json
+import socket
+
+def send_data(data):
+    HOST = '127.0.0.1'  # Change this to the IP address or hostname of your server
+    PORT = 9999  # Change this to the port number your server is running on
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(data.encode('utf-8'))  # Ensure data is encoded as bytes before sending
+        response = s.recv(1024).decode('utf-8')
+    return response
 
 def authenticate(username, password):
-    with open('data/users.json', 'r') as f:
-        users = json.load(f)
-
-    for user in users:
-        if (user['username'].upper() == username.upper() or user['email'].upper() == username.upper()) and user['password'] == password:
-            return True
-    return False
+    username = username.strip()
+    password = password.strip()
+    data = f"authenticate={username}={password}"
+    responce = send_data(data)
+    if responce[0:] == "success":
+        return True
+    else:
+        error_split = responce.split(",")
+        error_format = error_split[1][2:-2]
+        return False, error_format
